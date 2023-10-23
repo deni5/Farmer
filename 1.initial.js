fd3a1fd425d405bf3a6926122c97151d3e44b9c8
DENIS <seito@ukr.net>
2023-10-23 11:06:57 +0300

    Update 1_initial.js

--- 1.initial.js
+++ 1.initial.js
@@ -1,55 +1,56 @@
 var Farmer = artifacts.require("Farmer");
 var Wolf = artifacts.require("Wolf");
 
-async function addAlimal (address){
-    let result = await farmer.addAnimal(address);
-}
-async function addAlimal (address, food){
-    let result = await farmer.addAnimal(address);
-    console.log("feedAnimal result", result);
-}
-
-module.exports = async (deployer)=>{
-}
-
+module.exports = async function(deployer) {
     let farmer = null;
- //   try{
-    try{
+    let wolf = null;
+
+    try {
+        farmer = await Farmer.deployed();
+    } catch (e) {
+        await deployer.deploy(Farmer);
+        farmer = await Farmer.deployed();
     }
-        farmer = await Farmer.deployed()
- //   }catch(e){
-    await deployer.deploy(Farmer);
- 
- if(!farmer){
-    farmer = await Farmer.deployed();
- }
- console.log('continue execution');
-    console.log(farmer.address);
 
- try{
-        wolf = await Wolf.deployed()
- //   }catch(e){
-    await deployer.deploy(Wolf,"Akella");
- }
- if(!wolf){
-    wolf = await Wolf.deployed();
- }
+    console.log('continue execution');
+    console.log('Farmer address:', farmer.address);
 
-addAnimal(wolf.address);
- ctch(e){
-    console.error(e);
- }
+    try {
+        wolf = await Wolf.deployed();
+    } catch (e) {
+        await deployer.deploy(Wolf, "Akella");
+        wolf = await Wolf.deployed();
+    }
 
-await addAnimal(wolf.address);
-await feedAnimal(wolf.address, "meat");
-await callAnimal(wolf.address);
-await feedAnimal(wolf.address, "plant");
- ctch(e){
-    console.error(e);
- }
+    async function addAnimal(address) {
+        try {
+            let result = await farmer.addAnimal(address);
+            console.log("addAnimal result", result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
- 
+    async function feedAnimal(address, food) {
+        try {
+            let result = await farmer.feed(address, food);
+            console.log("feedAnimal result", result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
- 
-    
+    async function callAnimal(address) {
+        try {
+            let result = await farmer.call(address);
+            console.log("callAnimal result", result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
+    await addAnimal(wolf.address);
+    await feedAnimal(wolf.address, "meat");
+    await callAnimal(wolf.address);
+    await feedAnimal(wolf.address, "plant");
+}
